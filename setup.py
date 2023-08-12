b9670ccb86d3e1236e5d507fd6d8c6d34ac802e4
Dark <61820492+The-DarK-os@users.noreply.github.com>
2023-08-12 20:45:20 +0530

    File-modif

--- setup.py
+++
@@ -1,8 +0,0 @@
-# from setuptools import setup
-# from setuptools import find_packages
-
-# setup(name="gymrl",version="0.1",description="Reinforcement Learning Algorithms",author="DarK",
-#       url="https://github.com/The-DarK-os/gymrl",
-#       license="MIT",
-#       install_requires=["gym","numpy","matplotlib","tensorflow"],
-#       packages=find_packages())
